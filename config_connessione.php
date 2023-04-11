9461082aa043f9a2b62b6368b1ead3e64a3498fe
Andrea Serrano <serranoandrea515@gmail.com>
2023-04-12 01:29:33 +0200

    Commit & push: setup php web server e mysql server su linux

--- config_connessione.php
+++ config_connessione.php
@@ -2,7 +2,7 @@
     //Avvia la sessione
     session_start();
     //Istanzia variabili con dati per connettere db
-    $connessione= "mysql:host=localhost;dbname=eformdb;charset=utf8mb4";
+    $connessione= "mysql:host=localhost;dbname=AnswerHUB_DB;charset=utf8mb4";
     $utente = "root";
     $password = "erT23.hk";
     //apri cartella da terminale ed esegui php -S localhost:8080
